fa15a16e97de07e53d9775607d9e053cf01d61a8
Goran <goran.schyns@student.hepl.be>
2021-09-05 23:56:16 +0200

    Trying to send emails

--- resources/views/emails/success.blade.php
+++ resources/views/emails/success.blade.php
@@ -1,8 +1,10 @@
 @component('mail::message')
     # Merci pour votre commande !
 
+    {{$content}}
+
     Nous vous remercions d'avoir réservé vos entrées pour le Marché des Gourmets.
 
-    En espérant vous revoir rapidement,<br>
+    En espérant vous revoir rapidement,
     Le Marché des Gourmets
 @endcomponent
