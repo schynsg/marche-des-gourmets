ebbbf8fae7a39126f465b7557d83da412b66e4c0
Goran <goran.schyns@student.hepl.be>
2021-09-06 00:13:26 +0200

    Trying to send emails with params

--- resources/views/emails/success.blade.php
+++ resources/views/emails/success.blade.php
@@ -1,9 +1,14 @@
 @component('mail::message')
     # Merci pour votre commande !
 
-    {{$content}}
+    Bonjour {{$content->first_name}} {{$content->last_name}},
 
     Nous vous remercions d'avoir réservé vos entrées pour le Marché des Gourmets.
+
+    Nombre d'entrées adultes : {{$content->number}}
+    Nombre d'entrées enfants : {{$content->number_kids}}
+
+    Ce mail fait office de ticket d'entrée le jour du marché. Veuillez donc le présenter à l'entrée.
 
     En espérant vous revoir rapidement,
     Le Marché des Gourmets
